950420451927e70b348af0cf62e34c06c2ebed26
Praveen Kumar <praveenkumar@MacBook-Pro.local>
2020-08-26 12:18:15 +0530

    more changes

--- src/NumberInput.tsx
+++ src/NumberInput.tsx
@@ -15,7 +15,9 @@ class NumberInput extends React.Component<any, any>{
     const scale: string = this.props.scale;
     return (
       <div>
+        <div>This is the test line</div>
         <p>Enter the {scale} value here to process</p>
+        <div>This is another test line</div>
         <input value={num}
           onChange={this.handleChange} />
       </div>
